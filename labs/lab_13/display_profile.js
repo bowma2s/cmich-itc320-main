748381cc8e1db9fad298e1b814bb321ffddec822
bowma2s <bowma2s@cmich.edu>
2023-12-01 00:24:37 -0500

    lab 13

--- labs/lab_13/display_profile.js
+++ labs/lab_13/display_profile.js
@@ -2,8 +2,18 @@
 
 $(document).ready( () => {
 	// display data from session storage
+
+	$("#email").text(sessionStorage.getItem("email"));
+
+ 	$("#phone").text(sessionStorage.getItem("phone"));
+
+	$("#zip").text(sessionStorage.getItem("zip"));
+
+ 	$("#dob").text(sessionStorage.getItem("dob"));
 	
-	$("back").click( () => {
+	$("#back").click( () => {
+
+		history.back();
 		
 	}); // end of click()
 	
